4cf5a5d482df237d00e50fd4ff2e007a35983d8a
Universe-l <2446481918@qq.com>
2020-05-02 22:24:09 +0800

    program initial

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="head">
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.head
+  width 100%
+  
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Cart from "../pages/Cart.vue";
+import Address from "../pages/Address.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Cart",
+    component: Cart
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/Address",
+    name: "Address",
+    component: Address
   }
 ];
 
